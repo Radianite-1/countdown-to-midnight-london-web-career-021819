f5cdd85b19950593259d2ff23b714cab4fb87d79
Luke Ojukwu <luke.ojukwu@gmail.com>
2019-01-27 16:05:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  
+  counter = number
+  
+  while int > 0
+  
+  puts "#{number} SECOND(S)!"
+  
+  counter -= 1
+  
+  return "HAPPY NEW YEAR!"
+  
+  end
+
+  
 end
